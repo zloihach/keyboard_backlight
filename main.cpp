935cf6985dd23d6d3ccddfad6a5bdfdec2aaba29
FSB-PC <homkkarsx@gmail.com>
2022-11-03 10:41:14 +0300

    last ver

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-const int size = 10;
-//DWORD arr[size];
 void SetKey(BOOL bState, DWORD key) {
     BYTE keyState[256];
 
@@ -25,21 +23,60 @@ void SetKey(BOOL bState, DWORD key) {
     }
 }
 
-int setDelay(){
-    int delay;
-    cout << "Enter delay: ";
-    cin >> delay;
+//int setDelay(){
+//    static int delay;
+//    cout << "Enter delay: ";
+//    cin >> delay;
+//    return delay;
+//}
+//
+//int delayController(){
+//    static int delay = 250;
+//        if (GetAsyncKeyState(VK_OEM_PLUS) && delay <=5000){
+//            delay +=250;
+//            return delay;
+//            cout << "delay: " << delay << endl;
+//
+//        }
+//        if (GetAsyncKeyState(VK_OEM_MINUS) && delay >=0){
+//            delay -= 250;
+//            return delay;
+//            cout << "delay: " << delay << endl;
+//
+//        }
+//        else{
+//            cout << "Except!" << endl;
+//            exit(0);
+//        }
+//}
+
+DWORD handleCtl(void)
+{
+    const int dt = 100;
+    static int delay = 1000;
+    if (GetAsyncKeyState(VK_OEM_PLUS) && delay <= 5000){
+        delay += dt;
+        cout << "delay: " << delay << endl;
+    }
+    if (GetAsyncKeyState(VK_OEM_MINUS) && delay >= 0){
+        delay -= dt;
+        cout << "delay: " << delay << endl;
+    }
+    if (delay < dt) delay = dt;
     return delay;
 }
 
-
 int main() {
     int a = 1;
-    int delay = setDelay();
+    cout << "Enter Crtl+C for close this program" << endl;
+    //int delay = delayController();
+    DWORD delay = 3000;
     while (a = 1 && delay > 0)  {
         SetKey(TRUE, VK_CAPITAL);
         Sleep(delay);
+        delay = handleCtl();
         SetKey(FALSE, VK_CAPITAL);
         Sleep(delay);
+        delay = handleCtl();
     }
 }
